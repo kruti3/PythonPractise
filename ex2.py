91cdb45c0a795903a6fd878e72c7d7e8aa662b74
janhavi2004 <janhavi.k2004@gmail.com>
2014-07-26 23:54:38 +0530

    Exercise for commandline arguments

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#command line arguments
+from sys import argv
+
+script, one, two, three=argv
+print "The script is called ", script
+print "Command line arguments are: %r %r %r" %(one, two, three)
+

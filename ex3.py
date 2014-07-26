bad5c9c564410880c53c06782fcc8302e329c328
janhavi2004 <janhavi.k2004@gmail.com>
2014-07-27 01:23:16 +0530

    ex.16

--- ex3.py
+++ ex3.py
@@ -1,7 +1,26 @@
-print "Enter the filename: "
-name=raw_input("> ")
-txt=open(name)
+#Program for writing into file. Ex #16
+print "Enter the filename:",
+filename=raw_input()
 
-print "The contents of the file are:"
+print "Opening the file..."
+print "text in the file:"
+name=open(filename)
+print name.read()
+name.close()
+print "Opening file in write mode..."
+name=open(filename,'w')
 
-print txt.read()
+print name.truncate()
+
+print "Enter three lines of your choice:"
+line1=raw_input("Line 1: ")
+line2=raw_input("Line 2: ")
+line3=raw_input("Line 3: ")
+print "Writing these lines to the file..."
+ 
+name.write(line1+"\n")
+name.write(line2+"\n")
+name.write(line3+"\n")
+print "Closing the file..."
+name.close()
+
